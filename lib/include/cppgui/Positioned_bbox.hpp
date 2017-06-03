ba7eacce10c58437b78b35ecf5b330aab5349947
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-06-03 13:02:05 +0200

    Positioned_bbox gained a few placement methods (on top of the alignment methods added earlier)

--- lib/include/cppgui/Positioned_bbox.hpp
+++ lib/include/cppgui/Positioned_bbox.hpp
@@ -23,6 +23,15 @@ namespace cppgui
         auto middle() const { return orig.y - bbox.y_max + bbox.height() / 2; }
         auto bottom() const { return orig.y - bbox.y_min; }
 
+        auto& set_left_edge_to  (Position x) { orig.x = x - bbox.x_min; return *this; }
+        auto& set_origin_to     (Position x) { orig.x = x; return *this; }
+        auto& set_center_to     (Position x) { orig.x = x - bbox.width() / 2 - bbox.x_min; return *this; }
+        auto& set_right_edge_to (Position x) { orig.x = x - bbox.x_max; return *this; }
+        auto& set_top_edge_to   (Position y) { orig.y = y - bbox.y_max; return *this; }
+        auto& set_baseline_to   (Position y) { orig.y = y; return *this; }
+        auto& set_middle_to     (Position y) { orig.y = y - bbox.height() / 2 + bbox.y_max; return *this; }
+        auto& set_bottom_to     (Position y) { orig.y = y + bbox.y_min; return *this; }
+
         auto& align_origin_to  (Pbbox_cref ref) { orig.x = ref.orig.x; return *this; }
         auto& align_left_to    (Pbbox_cref ref) { orig.x = ref.left  () - bbox.x_min; return *this; }
         auto& align_right_to   (Pbbox_cref ref) { orig.x = ref.right () - bbox.x_max; return *this; }
--- lib/src/Positioned_bbox.cpp
+++ lib/src/Positioned_bbox.cpp
@@ -3,5 +3,5 @@
 
 namespace cppgui
 {
-    
+
 } // ns cppgui
