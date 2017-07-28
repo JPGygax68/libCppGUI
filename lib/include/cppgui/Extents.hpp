1b987bb7606dd4ec6fda1b089530eb754268154b
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-07-28 15:19:00 +0200

    Extents has gained an aspect() query

--- lib/include/cppgui/Extents.hpp
+++ lib/include/cppgui/Extents.hpp
@@ -11,9 +11,11 @@ namespace cppgui {
 
         Extents(Length w_, Length h_): w{w_}, h{h_} {}
         Extents() = default;
-        Extents(Bbox_cref );
+        Extents(Bbox_cref);
 
         auto operator |=(const Extents &e) -> Extents &;
+
+        auto aspect() const { return static_cast<float>(w) / static_cast<float>(h); }
     };
 
 } // ns cppgui
