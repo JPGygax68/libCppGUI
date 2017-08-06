2ce77ce3ac0a19b66d3a8090db4f5e525e89ad0c
Jean-Pierre Gygax <gygax@practicomp.ch>
2017-08-06 18:19:35 +0200

    Added "settings" to conanfile

--- conanfile.py
+++ conanfile.py
@@ -3,6 +3,8 @@ from conans import ConanFile
 
 class CppGUI(ConanFile):
     requires = "gpcbin2c/0.1.1@jpgygax68/testing"
+    settings = "os", "compiler", "build_type", "arch"
+
     generators = "virtualenv"
 
     #def build_requirements(self):
